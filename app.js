52980b8101d3be85fcc0326022a2c613e4aac198
zunshengbajian <zunshengbajian@gmail.com>
2022-07-04 12:36:41 +0300

    add project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('Hei, ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('36') // change this string to ensure a new version deployed
+})
